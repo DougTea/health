7870d508cac6e946b4e736b575079ed075acd054
guangyue <609440700@qq.com>
2020-02-23 18:40:57 +0800

    code save

--- src/main/java/io/transwarp/health/service/impl/HealthServiceImpl.java
+++ src/main/java/io/transwarp/health/service/impl/HealthServiceImpl.java
@@ -14,6 +14,8 @@ import org.apache.hadoop.hbase.util.Pair;
 import org.apache.hadoop.hyperbase.datatype.PrimaryDataType;
 import org.apache.hadoop.hyperbase.datatype.StringHDataType;
 import org.apache.hadoop.hyperbase.datatype.StructHDataType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -21,20 +23,32 @@ import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Shannon on 2020/2/23.
  */
 @Service
 public class HealthServiceImpl implements HealthService {
+  public static final Logger LOG = LoggerFactory.getLogger(HealthServiceImpl.class);
   @Resource
   HConnection hConnection;
+
+
+  private AtomicLong counter = new AtomicLong(0);
 
   @Resource
   HbaseClientProperties hbaseClientProperties;
 
   @Override
   public HealthResponse getHealth(String xm, String zjxm) {
+
+
+    long startTime = System.currentTimeMillis();
+    long time1 = startTime;
+    long time2 = startTime;
+    long time3 = startTime;
+    boolean success = true;
 
     TableName tableName = TableName.valueOf(hbaseClientProperties.getDbName(), hbaseClientProperties.getTableName());
     HealthResponse response = new HealthResponse();
@@ -55,22 +69,43 @@ public class HealthServiceImpl implements HealthService {
       // 1. 获取表数据
       Result result = hTable.get(new Get(structValue));
       // 2. 获取rk
+      time1 = System.currentTimeMillis();
+
       byte[] rk = result.getRow(); // rk其实就是 new Get里面的(structValue)
       // 3. 获取某一列的值,具体的可以从desc formatted table中看column mappings
       StringHDataType stringHDataType = new StringHDataType();
 
+      time2 = System.currentTimeMillis();
+
       byte[] type = result.getValue(Bytes.toBytes("f"), Bytes.toBytes("a3"));
+      if (type == null || type.length == 0) {
+        response.setType(HealthType.GREEN.getCode());
+        counter.incrementAndGet();
+        return response;
+      }
       String sType = stringHDataType.decode(type);
+      time3 = System.currentTimeMillis();
 
       if (sType.equals(HealthType.GREEN.getCode()) || sType.equals(HealthType.YELLOW.getCode())
         || sType.equals(HealthType.RED.getCode()) || sType.equals(HealthType.NOT_FOUND.getCode())) {
+
         response.setType(sType);
       } else {
         response.setType(HealthType.GREEN.getCode());
       }
     } catch (IOException e) {
+      LOG.error("IO Exception", e);
+      success = false;
       response.setType(HealthType.GREEN.getCode());
     }
+
+    counter.incrementAndGet();
+    long period1 = time1 - startTime;
+    long period2 = time2 - time1;
+    long period3 = time3 - time2;
+    LOG.info("request {}, success = {}, period1 = {}, period2 = {}, period3 = {}", counter.get(), success, period1, period2, period3);
+
+
     return response;
   }
 }
