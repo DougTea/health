df2d548a26faa3dd9f14ac1c8ea9c0e36fffc498
jiangzhe <zhe.jiang@transwarp.io>
2020-02-24 08:29:09 +0800

    add prometheus

---
+++ src/main/java/io/transwarp/health/monitoring/MonitoringConfiguration.java
@@ -0,0 +1,35 @@
+package io.transwarp.health.monitoring;
+
+import io.prometheus.client.exporter.MetricsServlet;
+import io.prometheus.client.hotspot.DefaultExports;
+import io.prometheus.client.spring.boot.SpringBootMetricsCollector;
+import org.springframework.boot.actuate.endpoint.PublicMetrics;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collection;
+
+@Configuration
+public class MonitoringConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    SpringBootMetricsCollector springBootMetricsCollector(Collection<PublicMetrics> publicMetrics) {
+
+        SpringBootMetricsCollector springBootMetricsCollector = new SpringBootMetricsCollector(publicMetrics);
+        springBootMetricsCollector.register();
+        return springBootMetricsCollector;
+    }
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        DefaultExports.initialize();
+        return new ServletRegistrationBean(new MetricsServlet(), "/prometheus");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestStatsInterceptor());
+    }
+}
